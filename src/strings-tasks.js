cdec02e69101eab3a953ea41ccc0be9d866346f5
Stanislav Berestetskyi <stasberestetskiy@gmail.com>
2023-11-17 15:06:37 +0100

    all tasks completed

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -288,8 +288,8 @@ function orderAlphabetically(str) {
  *   containsSubstring('JavaScript is Fun', 'Python') => false
  *   containsSubstring('12345', '34') => true
  */
-function containsSubstring(/* str, substring */) {
-  throw new Error('Not implemented');
+function containsSubstring(str, substring) {
+  return str.includes(substring);
 }
 
 /**
@@ -306,8 +306,11 @@ function containsSubstring(/* str, substring */) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowelRegex = /[aeiouyAEIOUY]/g;
+  const matches = str.match(vowelRegex);
+
+  return matches ? matches.length : 0;
 }
 
 /**
@@ -323,8 +326,9 @@ function countVowels(/* str */) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  return cleanStr === cleanStr.split('').reverse().join('');
 }
 
 /**
@@ -339,8 +343,16 @@ function isPalindrome(/* str */) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  const words = sentence.split(' ');
+
+  const longestWord = words.reduce((longest, current) => {
+    const cleanWord = current.replace(/[^a-zA-Z]/g, '');
+
+    return cleanWord.length > longest.length ? cleanWord : longest;
+  }, '');
+
+  return longestWord;
 }
 
 /**
@@ -353,8 +365,16 @@ function findLongestWord(/* sentence */) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const words = str.split(' ');
+
+  const reversedWords = words.map((word) => {
+    return word.split('').reverse().join('');
+  });
+
+  const reversedString = reversedWords.join(' ');
+
+  return reversedString;
 }
 
 /**
@@ -368,8 +388,13 @@ function reverseWords(/* str */) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  return str
+    .split('')
+    .map((char) =>
+      char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase()
+    )
+    .join('');
 }
 
 /**
@@ -385,8 +410,8 @@ function invertCase(/* str */) {
  *   getStringFromTemplate('John','Doe') => 'Hello, John Doe!'
  *   getStringFromTemplate('Chuck','Norris') => 'Hello, Chuck Norris!'
  */
-function getStringFromTemplate(/* firstName, lastName */) {
-  throw new Error('Not implemented');
+function getStringFromTemplate(firstName, lastName) {
+  return `Hello, ${firstName} ${lastName}!`;
 }
 
 /**
@@ -399,8 +424,13 @@ function getStringFromTemplate(/* firstName, lastName */) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const commaIndex = value.indexOf(',');
+  const exclamationIndex = value.indexOf('!');
+
+  const name = value.slice(commaIndex + 2, exclamationIndex).trim();
+
+  return name;
 }
 
 /**
@@ -414,8 +444,8 @@ function extractNameFromTemplate(/* value */) {
  *   unbracketTag('<span>') => 'span'
  *   unbracketTag('<a>') => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  return str.slice(1, -1);
 }
 
 /**
@@ -433,8 +463,12 @@ function unbracketTag(/* str */) {
  *   ],
  *   'info@gmail.com' => ['info@gmail.com']
  */
-function extractEmails(/* str */) {
-  throw new Error('Not implemented');
+function extractEmails(str) {
+  const emailsArray = str.split(';');
+
+  const trimmedEmails = emailsArray.map((email) => email.trim());
+
+  return trimmedEmails;
 }
 
 /**
@@ -453,8 +487,32 @@ function extractEmails(/* str */) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
+function encodeToRot13(str) {
+  const rot13Transform = (char) => {
+    const isUpperCase = /[A-Z]/.test(char);
+    const alphabet = isUpperCase
+      ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      : 'abcdefghijklmnopqrstuvwxyz';
+
+    const index = alphabet.indexOf(char);
+
+    if (index === -1) {
+      // Character is not in the alphabet, leave it unchanged
+      return char;
+    }
+
+    // Apply ROT13 transformation
+    const transformedIndex = (index + 13) % 26;
+    return alphabet[transformedIndex];
+  };
+
+  // Apply ROT13 transformation to each character in the input string
+  const result = str
+    .split('')
+    .map((char) => rot13Transform(char))
+    .join('');
+
+  return result;
 }
 
 /**
@@ -481,8 +539,65 @@ function encodeToRot13(/* str */) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const initialDeck = [
+    'A♣',
+    '2♣',
+    '3♣',
+    '4♣',
+    '5♣',
+    '6♣',
+    '7♣',
+    '8♣',
+    '9♣',
+    '10♣',
+    'J♣',
+    'Q♣',
+    'K♣',
+    'A♦',
+    '2♦',
+    '3♦',
+    '4♦',
+    '5♦',
+    '6♦',
+    '7♦',
+    '8♦',
+    '9♦',
+    '10♦',
+    'J♦',
+    'Q♦',
+    'K♦',
+    'A♥',
+    '2♥',
+    '3♥',
+    '4♥',
+    '5♥',
+    '6♥',
+    '7♥',
+    '8♥',
+    '9♥',
+    '10♥',
+    'J♥',
+    'Q♥',
+    'K♥',
+    'A♠',
+    '2♠',
+    '3♠',
+    '4♠',
+    '5♠',
+    '6♠',
+    '7♠',
+    '8♠',
+    '9♠',
+    '10♠',
+    'J♠',
+    'Q♠',
+    'K♠',
+  ];
+
+  const cardId = initialDeck.indexOf(value);
+
+  return cardId;
 }
 
 module.exports = {
